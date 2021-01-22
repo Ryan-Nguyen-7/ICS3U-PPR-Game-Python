af7ecf549c7ac59e46da973ed023011c492253c7
Ryan-Nguyen-7 <74927146+Ryan-Nguyen-7@users.noreply.github.com>
2021-01-21 21:14:23 -0500

    Create constants.py

---
+++ Master/constants.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Nguyen
+# Created on January 2021
+# This constants file is for Prance Prance Rebellion game
+
+# PyBadge screen size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+
+TOTAL_NUMBER_OF_NOTES = 4
+TOTAL_NUMBER_OF_LASERS = 5
+STARTING_NOTE_SPEED = 2
+
+OFF_SCREEN_X = -100
+OFF_SCREEN_Y = -100
+OFF_TOP_SCREEN = -1 * SPRITE_SIZE
+OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+# Using for button state
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}

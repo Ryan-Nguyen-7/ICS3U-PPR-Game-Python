b38282fd69e8ddb7a0c3c05ff3352790d8e143ab
Ryan-Nguyen-7 <74927146+Ryan-Nguyen-7@users.noreply.github.com>
2021-01-25 14:20:38 -0500

    Update code.py

--- Master/code.py
+++ Master/code.py
@@ -1,23 +1,65 @@
 #!/usr/bin/env python3
 
 # Created by Ryan Nguyen
-# Created on January 2021
-# This program is the "Prance Prance Rebellion" game on the Pybadge
+# Created on November 2020
+# This program is the "Space Aliens" program on the Pybadge
+
 
 import ugame
 import stage
-import time
-import random
-import supervisor
-
-import constants
-
 
 def splash_scene():
     # this function is the splash scene
-    
-    game_scene()
-    
+
+    # image banks for CircuitPython
+    splash_screen_background = stage.Bank.from_bmp16("splash_screen.bmp")
+
+    # sets the background to image 0 in the image Bank
+    background = stage.Grid(splash_screen_background, constants.SCREEN_X,
+                            constants.SCREEN_Y)
+
+    # background made up of 16 sprites and their coordinates
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers, items will show up in order
+    game.layers = [background]
+    # render the background and initial location of sprite list
+    # most likely you will only render background once per scene
+    game.render_block()
+
+    # calls menu scene
+    while True:
+        time.sleep(2.0)
+        menu_scene()
 def game_scene():
     # this function is the main game game_scene
   
@@ -46,6 +88,26 @@ def game_scene():
                 break
 
     # image banks for CircuitPython
-    
+
 if __name__ == "__main__":
     splash_scene()
+def game_scene():
+    # this function is the main game game_scene
+
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
+    
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [background]
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+        pass #  just a placeholder for now
+
+
+if __name__ == "__main__":
+    game_scene()
+
+
+

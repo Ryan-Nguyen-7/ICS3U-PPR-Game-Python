5e3421ed85f64cb57824ac4144a8e351da8a0f7e
Ryan-Nguyen-7 <74927146+Ryan-Nguyen-7@users.noreply.github.com>
2021-01-27 00:10:04 -0500

    Update code.py

--- Lesson03/code.py
+++ Lesson03/code.py
@@ -55,7 +55,7 @@ def splash_scene():
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
     game = stage.Stage(ugame.display, constants.FPS)
-    # set the layers; right now, it is just the splah screen
+    # set the layers; right now, it is just the splash screen
     game.layers = [background]
     # render the background and initial location of sprite list
     game.render_block()
@@ -70,29 +70,30 @@ def menu_scene():
     # this function is the menu scene
 
     # image banks for CircuitPython
-    image_bank = stage.Bank.from_bmp16("sprites_and_background.bmp")
+    image_bank = stage.Bank.from_bmp16("space_aliens.bmp")
 
     # add text objects
     text = []
+
     text1 = stage.Text(width=29, height=12, font=None, palette=constants.BLUE_PALETTE, buffer=None)
-    text1.move(20, 10)
+    text1.move(15, 10)
     text1.text("Big Brain Studios")
     text.append(text1)
 
     text2 = stage.Text(width=29, height=12, font=None, palette=constants.BLUE_PALETTE, buffer=None)
-    text2.move(40, 110)
+    text2.move(37, 110)
     text2.text("PRESS START")
     text.append(text2)
-    
+
     # sets the background to image 0 in the image Bank
-    menu_background = stage.Grid(image_bank, constants.SCREEN_X,
+    background = stage.Grid(image_bank, constants.SCREEN_X,
                             constants.SCREEN_Y)
 
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
     game = stage.Stage(ugame.display, constants.FPS)
     # set the layers, items will show up in order
-    game.layers = text + [menu_background]
+    game.layers = text + [background]
     # render the background and initial location of sprite list
     # most likely you will only render background once per scene
     game.render_block()
@@ -112,49 +113,66 @@ def menu_scene():
 def game_scene():
     # this function is the main game scene
 
-    # prepare sound
-    damage_sound = open("damage.wav", 'rb')
-    boop_sound = open("boop.wav", 'rb')
-    sound = ugame.audio
-    sound.stop()
-    sound.mute(False)
-    sound.play(boop_sound)
+    # health, score, combo
+    text = []
+
+    health = 3
+    health_text = stage.Text(width=29, height=14)
+    health_text.clear()
+    health_text.move(5 * constants.SPRITE_SIZE,0)
+    health_text.text("HP: {0}".format(health))
+    text.append(health_text)
+
+    score = 0
+    score_text = stage.Text(width=29, height=14)
+    score_text.clear()
+    score_text.move(5 * constants.SPRITE_SIZE,10)
+    score_text.text("Score: {0}".format(score))
+    text.append(score_text)
+
+    combo = 0
+    combo_text = stage.Text(width=29, height=14)
+    combo_text.clear()
+    combo_text.move(5 * constants.SPRITE_SIZE,20)
+    combo_text.text("Combo: {0}x".format(combo))
+    text.append(combo_text)
 
     # imports image bank .bmp file
-    image_bank = stage.Bank.from_bmp16("sprites_and_background.bmp")
+    image_bank = stage.Bank.from_bmp16("space_aliens_background.bmp")
 
     # sets the background to image 0 in the image Bank
     #   and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank, constants.SCREEN_GRID_X,
                             constants.SCREEN_GRID_Y)
 
-    # places the lanes on-screen
-    for x_location in range(1, 4):
-        for y_location in range(constants.SCREEN_GRID_Y - 1):
-            background.tile(x_location, y_location, 7)
+    # sets randomized starry background
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            tile_picked = random.randint(1, 3)
+            background.tile(x_location, y_location, tile_picked)
+
+    # sets the lanes
+    for x_location in range(1, 5):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            background.tile(x_location, y_location, 4) # 4th element = straight ship
 
     # places target pads
-    background.tile(1, y_location, 1)
-    background.tile(2, y_location, 2)
-    background.tile(3, y_location, 3)
-    background.tile(4, y_location, 4)
-    
-    # places the rest of the screen
-    for x_location in range(5, constants.SCREEN_GRID_Y):
-        for y_location in range(constants.SCREEN_GRID_Y):
-            background.tile(x_location, y_location, 8)
+    background.tile(1, constants.SCREEN_GRID_Y - 1, 8) # 8th element = chill squid
+    background.tile(2, constants.SCREEN_GRID_Y - 1, 8)
+    background.tile(3, constants.SCREEN_GRID_Y - 1, 8)
+    background.tile(4, constants.SCREEN_GRID_Y - 1, 8)
 
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
     # set the layers, items show up in order
-    game.layers = [background]
+    game.layers = text + [background]
     # render the background and initial location of sprite list
     game.render_block()
 
     # repeat forever, game loop
     while True:
-        game.tick()
+        pass # placeholder
 
 if __name__ == "__main__":
     splash_scene()
